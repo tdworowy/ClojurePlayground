343c9ce25f2095f168ead31f4a3ad32878dfdc46
Tomasz Dworowy <dworowytomasz@gmail.com>
2025-02-28 18:55:04 +0100

    update

--- book_exmples/the-divine-cheese-code/src/the_divine_cheese_code/core.clj
+++ book_exmples/the-divine-cheese-code/src/the_divine_cheese_code/core.clj
@@ -1,8 +1,7 @@
 (ns the-divine-cheese-code.core
   (:require [clojure.java.browse :as browse]
-            [the-divine-cheese-code.visualization.svg :refer [points]])
+            [the-divine-cheese-code.visualization.svg :refer [xml]])
   (:gen-class))
-
 
 (def heists [{:location "Cologne, Germany"
               :cheese-name "Archbishop Hildebold's Cheese Pretzel"
@@ -25,6 +24,19 @@
               :lat 41.90
               :lng 12.45}])
 
+(defn url [filename]
+  (str "file:///" (System/getProperty "user.dir") "/" filename))
+
+(defn template
+  [contents]
+  (str "<style>polyline { fill:none; stroke:#5881d8; stroke-width:3}</style>"
+       contents))
+
 (defn -main [& args]
-  (println (points heists))
-  )
+  (let [filename "map.html"]
+    (->> heists
+         (xml 50 100)
+         template
+         (spit filename))
+    ;(browse/browse-url (url filename))))
+    (println (url filename))))
